gridgentoo/bitwarden_server

Add cloud api key field for installation communication

util/MySqlMigrations/Migrations/DatabaseContextModelSnapshot.cs
@@ -475,6 +475,9 @@ namespace Bit.MySqlMigrations.Migrations
                         .HasMaxLength(30)
                         .HasColumnType("varchar(30)");
 
+                    b.Property<string>("CloudApiKey")
+                        .HasColumnType("longtext");
+
                     b.Property<DateTime>("CreationDate")
                         .HasColumnType("datetime(6)");
 

ysf/server

util/PostgresMigrations/Migrations/DatabaseContextModelSnapshot.cs
@@ -478,6 +478,9 @@ namespace Bit.PostgresMigrations.Migrations
                         .HasMaxLength(30)
                         .HasColumnType("character varying(30)");
 
+                    b.Property<string>("CloudApiKey")
+                        .HasColumnType("text");
+
                     b.Property<DateTime>("CreationDate")
                         .HasColumnType("timestamp without time zone");
 