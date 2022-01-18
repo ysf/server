4836e1e6decbb04ef99429e86dcb7d78ca069598
Hinton <oscar@oscarhinton.com>
2022-01-18 21:35:20 +0100

    Fix tests

--- test/Api.Test/Controllers/AccountsControllerTests.cs
+++ test/Api.Test/Controllers/AccountsControllerTests.cs
@@ -4,10 +4,7 @@ using System.Threading.Tasks;
 using Bit.Api.Controllers;
 using Bit.Api.Models.Request.Accounts;
 using Bit.Core.Entities;
-using Bit.Core.Enums;
 using Bit.Core.Exceptions;
-using Bit.Core.Models.Api.Request.Accounts;
-using Bit.Core.Models.Data;
 using Bit.Core.Repositories;
 using Bit.Core.Services;
 using Bit.Core.Settings;
@@ -27,7 +24,6 @@ namespace Bit.Api.Test.Controllers
         private readonly IOrganizationService _organizationService;
         private readonly IOrganizationUserRepository _organizationUserRepository;
         private readonly IPaymentService _paymentService;
-        private readonly IUserRepository _userRepository;
         private readonly IUserService _userService;
         private readonly ISendRepository _sendRepository;
         private readonly ISendService _sendService;
@@ -36,7 +32,6 @@ namespace Bit.Api.Test.Controllers
         public AccountsControllerTests()
         {
             _userService = Substitute.For<IUserService>();
-            _userRepository = Substitute.For<IUserRepository>();
             _cipherRepository = Substitute.For<ICipherRepository>();
             _folderRepository = Substitute.For<IFolderRepository>();
             _organizationService = Substitute.For<IOrganizationService>();
@@ -54,7 +49,6 @@ namespace Bit.Api.Test.Controllers
                 _organizationUserRepository,
                 _providerUserRepository,
                 _paymentService,
-                _userRepository,
                 _userService,
                 _sendRepository,
                 _sendService
@@ -64,77 +58,6 @@ namespace Bit.Api.Test.Controllers
         public void Dispose()
         {
             _sut?.Dispose();
-        }
-
-        [Fact]
-        public async Task PostPrelogin_WhenUserExists_ShouldReturnUserKdfInfo()
-        {
-            var userKdfInfo = new UserKdfInformation
-            {
-                Kdf = KdfType.PBKDF2_SHA256,
-                KdfIterations = 5000
-            };
-            _userRepository.GetKdfInformationByEmailAsync(Arg.Any<string>()).Returns(Task.FromResult(userKdfInfo));
-
-            var response = await _sut.PostPrelogin(new PreloginRequestModel { Email = "user@example.com" });
-
-            Assert.Equal(userKdfInfo.Kdf, response.Kdf);
-            Assert.Equal(userKdfInfo.KdfIterations, response.KdfIterations);
-        }
-
-        [Fact]
-        public async Task PostPrelogin_WhenUserDoesNotExist_ShouldDefaultToSha256And100000Iterations()
-        {
-            _userRepository.GetKdfInformationByEmailAsync(Arg.Any<string>()).Returns(Task.FromResult((UserKdfInformation)null));
-
-            var response = await _sut.PostPrelogin(new PreloginRequestModel { Email = "user@example.com" });
-
-            Assert.Equal(KdfType.PBKDF2_SHA256, response.Kdf);
-            Assert.Equal(100000, response.KdfIterations);
-        }
-
-        [Fact]
-        public async Task PostRegister_ShouldRegisterUser()
-        {
-            var passwordHash = "abcdef";
-            var token = "123456";
-            var userGuid = new Guid();
-            _userService.RegisterUserAsync(Arg.Any<User>(), passwordHash, token, userGuid)
-                        .Returns(Task.FromResult(IdentityResult.Success));
-            var request = new RegisterRequestModel
-            {
-                Name = "Example User",
-                Email = "user@example.com",
-                MasterPasswordHash = passwordHash,
-                MasterPasswordHint = "example",
-                Token = token,
-                OrganizationUserId = userGuid
-            };
-
-            await _sut.PostRegister(request);
-
-            await _userService.Received(1).RegisterUserAsync(Arg.Any<User>(), passwordHash, token, userGuid);
-        }
-
-        [Fact]
-        public async Task PostRegister_WhenUserServiceFails_ShouldThrowBadRequestException()
-        {
-            var passwordHash = "abcdef";
-            var token = "123456";
-            var userGuid = new Guid();
-            _userService.RegisterUserAsync(Arg.Any<User>(), passwordHash, token, userGuid)
-                        .Returns(Task.FromResult(IdentityResult.Failed()));
-            var request = new RegisterRequestModel
-            {
-                Name = "Example User",
-                Email = "user@example.com",
-                MasterPasswordHash = passwordHash,
-                MasterPasswordHint = "example",
-                Token = token,
-                OrganizationUserId = userGuid
-            };
-
-            await Assert.ThrowsAsync<BadRequestException>(() => _sut.PostRegister(request));
         }
 
         [Fact]
